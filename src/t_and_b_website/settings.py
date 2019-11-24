258986b19ff9b621d5d9b92fed602b06c6f96de1
Alex Ren <alexren268@gmail.com>
2019-11-23 21:18:11 -0500

    edited static root

--- src/t_and_b_website/settings.py
+++ src/t_and_b_website/settings.py
@@ -155,9 +155,11 @@ USE_TZ = True
 
 #BASE_DIR is where manage.py lives
 STATIC_URL = '/static/'
+
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'staticfiles'), 
-]
+# ]
+
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 
 #MEDIA_ROOT is for directory for any media that our users upload
