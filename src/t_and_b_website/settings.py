4d0500993a29e6cea5884808675b3a718fb103cc
Alex Ren <alexren268@gmail.com>
2019-11-24 14:47:37 -0500

    set debug value

--- src/t_and_b_website/settings.py
+++ src/t_and_b_website/settings.py
@@ -37,7 +37,10 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG_VALUE = env('DEBUG_VALUE')
+
+#debug will be set to false if the env variable is anything other than 'True'
+DEBUG = (DEBUG_VALUE == "True")
 
 ALLOWED_HOSTS = ['127.0.0.1', 'testserver', 't-and-b-website.herokuapp.com']
 
