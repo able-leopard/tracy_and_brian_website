54bfc968ff53e5a732ed6e6645294fdbfdb28f43
Alex Ren <alexren268@gmail.com>
2019-12-08 23:29:37 -0500

    allowed csrf token to come through on incognito

--- src/frontend-app/src/cart/CartForm.js
+++ src/frontend-app/src/cart/CartForm.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "whatwg-fetch";
 import cookie from "react-cookies";
 
+
 /* 
 We have to make an initial get request to /api/cart/ to create a new cart if there are 
 no previous cart assoicated with this session.
@@ -23,9 +24,10 @@ class CartForm extends Component {
   // either adding or removing the item coming from data into cart send sending it to the REST api
   // Important to note that the paintingId must be in the currentPaintingId[] before sending the data through or else the backend won't know what to do
   // this.getCart() also gets called in this
-  updateCart = data => {
-    const endpoint = "/api/cart/update/";
+  updateCart = (data) => {
+    const endpoint = "/api/cart/update/"; 
     const csrfToken = cookie.load("csrftoken");
+    cookie.save('csrftoken', csrfToken);
 
     if (csrfToken !== undefined) {
       let lookupOptions = {
@@ -55,6 +57,7 @@ class CartForm extends Component {
   getCart = () => {
     const endpoint = `/api/cart/`;
     const csrfToken = cookie.load("csrftoken");
+    cookie.save('csrftoken', csrfToken);
 
     if (csrfToken !== undefined) {
       let lookupOptions = {
@@ -64,14 +67,14 @@ class CartForm extends Component {
           "X-CSRFToken": csrfToken
         },
         credentials: "include"
-      };
+    };
       fetch(endpoint, lookupOptions)
         .then(response => {
           return response.json();
         })
         .then(responseData => {
           this.setState({
-            currentCart: responseData.products
+            currentCart: responseData.products,
           });
         })
         .catch(error => {
@@ -81,61 +84,67 @@ class CartForm extends Component {
     }
   };
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
     let data = this.state;
 
-    if (data !== undefined) {
-      this.updateCart(data), console.log(this.state.currentCart);
+    if (data !== undefined){
+        (
+          this.updateCart(data)
+        )
     } else {
-      ("");
+        ""
     }
   };
 
   defaultState = () => {
-    this.setState({
-      currentPaintingId: []
-    });
-  };
+      this.setState({
+        currentPaintingId: [],
+      })
+  }
 
   componentDidMount() {
     // getting the current painting's ID
     const { paintingId } = this.props;
 
     // getting the items currently in the cart and storing in this.state.currentCart
-    this.getCart();
+    this.getCart()
 
     if (paintingId !== undefined) {
       // putting the current painting id into the currentPaintingId so the cart can associate what painting is being added later
       this.setState({
-        currentPaintingId: [paintingId]
+        currentPaintingId: [paintingId],
       });
     } else {
-      this.defaultState();
+      this.defaultState()
     }
   }
 
   render() {
-    // Get rid of the remove form cart later
 
-    const { currentCart, currentPaintingId } = this.state;
-    const myCart = [];
-    currentCart.forEach(function(item) {
-      myCart.push(item.id);
+    // Get rid of the remove form cart later
+    const {currentCart, currentPaintingId} = this.state
+
+
+    const myCart = []
+    currentCart.forEach(function (item) {
+      myCart.push(item.id)
     });
 
     return (
       <form onSubmit={this.handleSubmit}>
-        {myCart.includes(currentPaintingId[0]) ? (
+      
+        {
+          myCart.includes(currentPaintingId[0]) ? 
           <div>
-            <br />
-            <a>Current painting already in cart</a>
+            <br/>
+            <a>Current painting already in cart</a> 
           </div>
-        ) : (
+          : 
           <div>
             <button className="btn btn-primary">Add to cart</button>
           </div>
-        )}
+        }
       </form>
     );
   }
